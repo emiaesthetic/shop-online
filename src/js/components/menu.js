bd5a450778268c51fc1c89847fc841af54ddbee3
emiaesthetic <emi.aesthetic@gmail.com>
2024-10-25 07:27:17 +0500

    Add dynamic loading categories in menu

---
+++ src/js/components/menu.js
@@ -0,0 +1,81 @@
+import { loadCategories } from '../services/api.js';
+
+const isOpen = elem => elem.classList.contains('is-open');
+
+const setHiddenAttribute = (target, element) => {
+  target.setAttribute('aria-hidden', !isOpen(element));
+};
+
+const setExpandedAttribute = (target, element) => {
+  target.setAttribute('aria-expanded', isOpen(element));
+};
+
+const updateAriaHidden = dropdowns => {
+  // console.log(dropdowns);
+  dropdowns.forEach(item => {
+    const content = item.querySelector('.dropdown__content');
+
+    if (window.innerWidth >= 576) {
+      content.setAttribute('aria-hidden', 'true');
+    } else {
+      content.setAttribute('aria-hidden', 'false');
+    }
+  });
+};
+
+const createMenuItem = category => {
+  const item = document.createElement('li');
+  item.classList.add('menu__subitem');
+
+  const link = document.createElement('a');
+  link.classList.add('menu__sublink', 'underline-link');
+  link.href = '#';
+  link.textContent = category;
+
+  item.append(link);
+  return item;
+};
+
+const addCategoriesToCatalog = (catalog, categories) => {
+  const items = categories.map(category => createMenuItem(category));
+  catalog.append(...items);
+};
+
+const initMenu = async () => {
+  const menu = document.querySelector('.menu');
+  const menuList = document.querySelector('.menu__list');
+  const menuBtn = document.querySelector('.menu__button');
+  const dropdowns = document.querySelectorAll('.dropdown');
+  const [headerCatalog, footerCatalog] = document.querySelectorAll(
+    '.menu__sublist--columns-2',
+  );
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
+    setHiddenAttribute(menuList, menu);
+    setExpandedAttribute(menuBtn, menu);
+  });
+
+  dropdowns.forEach(item => {
+    const button = item.querySelector('.dropdown__button');
+    const content = item.querySelector('.dropdown__content');
+
+    button.addEventListener('click', () => {
+      button.classList.toggle('is-open');
+      setExpandedAttribute(button, button);
+
+      content.classList.toggle('is-open');
+      setHiddenAttribute(content, content);
+    });
+  });
+
+  window.addEventListener('resize', () => {
+    updateAriaHidden(dropdowns);
+  });
+
+  const categories = await loadCategories();
+  addCategoriesToCatalog(headerCatalog, categories);
+  addCategoriesToCatalog(footerCatalog, categories);
+};
+
+initMenu();
---
+++ src/js/services/api.js
@@ -0,0 +1,35 @@
+import { serverURL } from '../helpers/constants.js';
+
+const sendRequest = async (
+  path,
+  { method = 'GET', headers, body, callback },
+) => {
+  try {
+    const url = `${serverURL}${path}`;
+    const options = {
+      method,
+    };
+
+    if (headers) options.headers = headers;
+    if (body) options.body = JSON.stringify(body);
+
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error(`Ошибка ${response.status}`);
+
+    const data = await response.json();
+    if (callback) return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+};
+
+export const loadCategories = async () =>
+  await sendRequest('api/categories', {
+    callback: (error, categories) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      return categories;
+    },
+  });
